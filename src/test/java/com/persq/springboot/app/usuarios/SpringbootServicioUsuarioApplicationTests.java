2fcbf5d0caff28f7845eb6adc8e34c00c961da89
Luis Cochachi <lcochachi@gmail.com>
2022-10-13 18:25:34 -0500

    010

---
+++ src/test/java/com/persq/springboot/app/usuarios/SpringbootServicioUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persq.springboot.app.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
